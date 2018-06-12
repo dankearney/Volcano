abe3393560829b68445764df3c49c1d511cf29bc
dankearney <dank@LAPTOP-M7SECR63.localdomain>
2018-06-11 23:51:23 -0400

    started wireframing frontend chat and basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 
+import { FormsModule } from '@angular/forms';
+import { ChatPageComponent } from './chat-page/chat-page.component'; // <-- NgModel lives here
+
+import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './default/default.component';
+
+const appRoutes: Routes = [
+  { path: 'chat', component: ChatPageComponent },
+  { path: '', component: DefaultComponent }
+]
+
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    ChatPageComponent,
+    DefaultComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+    // other imports
   ],
   providers: [],
   bootstrap: [AppComponent]
