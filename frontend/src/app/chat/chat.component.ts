088dee2a8cfdb883665135a58582d781e0864d70
dankearney <dank@LAPTOP-M7SECR63.localdomain>
2018-06-11 23:51:23 -0400

    started wireframing frontend chat and basic routing

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Chat } from '../chat';
+import { MOCK_CHATS } from '../mock-chats'
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
+
 export class ChatComponent implements OnInit {
+
+  chats = MOCK_CHATS;
 
   constructor() { }
 
-  ngOnInit() {
+  // THIS IS A KLUDGE, DO NOT REPLICATE
+  onSubmit(event: any) { 
+  	let message = event.target[0].value;
+  	let newchat = new Chat('Dan', message);
+  	this.chats.push(newchat);
   }
 
-  chat = 'Chat';
+  ngOnInit() {
+  }
 
 }
