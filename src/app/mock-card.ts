6684ad5c44b8615e6e6e3a569e0c6969e02522bb
dankearney <dank@LAPTOP-M7SECR63.localdomain>
2018-06-12 20:53:07 -0400

    Added card and chat

---
+++ src/app/mock-card.ts
@@ -0,0 +1,16 @@
+import { Card } from './card';
+
+export const MOCK_CARD: Card = 
+{
+  number: 0,
+  title: 'Add header to mobile website', 
+  type: 'task',
+  story: 'story name',
+  priority: 'normal',
+  labels: 'mobile',
+  status: 'Not done',
+  resolution: '',
+  description: '',
+  attachment: '',
+  owner: 'Qian'
+};
