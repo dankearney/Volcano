e5a6ece2e2e460cc05488c41963a5ba1eea2a5c5
dankearney <dank@LAPTOP-M7SECR63.localdomain>
2018-06-12 20:53:07 -0400

    Added card and chat

---
+++ frontend/src/app/card.ts
@@ -0,0 +1,13 @@
+export class Card {
+  number: number;
+  title: string;
+  type: string;
+  story: string;
+  priority: string;
+  labels: string;
+  status: string;
+  resolution: string;
+  description: string;
+  attachment: string;
+  owner: string;
+}
