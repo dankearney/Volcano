dac40e32b26d7fcf2fda24beccec58f69989e16d
dankearney <dank@LAPTOP-M7SECR63.localdomain>
2018-06-12 20:42:30 -0400

    added card functionality

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MOCK_CARD } from '../mock-card.ts';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
+
 export class CardComponent implements OnInit {
+
+  card = MOCK_CARD;
 
   constructor() { }
 
