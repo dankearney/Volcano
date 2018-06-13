b5b740ed235f56dfc3f6e293005d37a6b9c2fa52
LimHengGuang <LimHengGuang@users.noreply.github.com>
2018-06-12 23:37:41 -0400

    Previous front-end bug fixed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,22 +8,11 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loginUserData = {}
-  constructor(
-    private _auth: AuthService,
-    private _router: Router) { }
+  constructor(private _router: Router) { }
 
   ngOnInit() {
   }
 
   loginUser() {
-    this._auth.loginUser(this.loginUserData)
-      .subscribe(
-        res => {
-          console.log(res)
-          localStorage.setItem('token', res.token)
-          this._router.navigate(['./special'])
-        },
-        err => console.log(err)
-      )
   }
 }
