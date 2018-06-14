9c0406df61fa2652af92d1d6b14a494df19f87b7
dankearney <dank@outlook.com>
2018-06-13 22:07:19 -0400

    added missing mock cards file

---
+++ frontend/src/app/mock-cards.ts
@@ -0,0 +1,43 @@
+import { Card } from './card';
+
+export const MOCK_CARDS: Card[] = [
+{
+  number: 1,
+  title: 'Add header to mobile site',
+  type: 'Task',
+  story: 'Story Name',
+  priority: 'Normal',
+  labels: 'Mobile',
+  status: 'Not Done',
+  resolution: '',
+  description: 'This is a demo description.',
+  attachment: '',
+  owner: 'Qian'
+},
+{
+  number: 5,
+  title: 'Make site responsive',
+  type: 'Task',
+  story: 'Story Name',
+  priority: 'Normal',
+  labels: 'Mobile',
+  status: 'Not Done',
+  resolution: '',
+  description: 'This is a demo description.',
+  attachment: '',
+  owner: 'Terence'
+},
+{
+  number: 7,
+  title: 'Localize for Spanish',
+  type: 'Task',
+  story: 'Story Name',
+  priority: 'Low',
+  labels: 'Localization',
+  status: 'Not Done',
+  resolution: '',
+  description: 'This is a demo description.',
+  attachment: '',
+  owner: 'Dan'
+},
+]
