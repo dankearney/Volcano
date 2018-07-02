87ba83a4668106ccec2dcd34ffa1135adba1dac6
dankearney <dank@LAPTOP-M7SECR63.localdomain>
2018-07-02 13:36:39 -0400

    chat

--- src/app/mock-chats.ts
+++ src/app/mock-chats.ts
@@ -1,16 +1,4 @@
 import { Chat } from './chat';
 
 export const MOCK_CHATS: Chat[] = [
-  {
-    message: 'Hello, World!'
-  },
-  {
-    message: 'Hello to you'
-  },
-  {
-    message: 'You as well!'
-  },
-  {
-    message: 'The volcano is erupting!'
-  }
 ];
