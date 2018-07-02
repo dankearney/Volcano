caeeb28aab6089f58f769c9edacba47c446772d7
dankearney <dank@LAPTOP-M7SECR63.localdomain>
2018-07-02 11:47:51 -0400

    made the card hydration more efficient

--- backend/src/main/java/com/example/postgresdemo/repository/UserRepository.java
+++ backend/src/main/java/com/example/postgresdemo/repository/UserRepository.java
@@ -4,10 +4,14 @@ import com.example.postgresdemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Collection;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
     Optional<User> findOneByUserid(Long id);
+    
+    List<User> findByUseridIn(List<Long> ids);
 }
