212dae47548324925e8fd83912f517ed22a48468
qian <dingqiandd@gmail.com>
2018-07-03 17:46:05 -0400

    update create story element

--- src/app/create-story/create-story.component.ts
+++ src/app/create-story/create-story.component.ts
@@ -1,22 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
-import { Story } from '../story'
+import { Story } from '../story';
+import { HttpClient } from '@angular/common/http';
+import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+import { Util } from '../utilities/util';
+
 
 @Component({
   selector: 'app-create-story',
   templateUrl: './create-story.component.html',
   styleUrls: ['./create-story.component.css']
 })
+
 export class CreateStoryComponent implements OnInit {
+
+  createStoryData = {};
 
   public createStoryForm : FormGroup;
 
-  constructor() { }
+  constructor(private _router: Router, private http: HttpClient) { }
 
   ngOnInit() {
-  }
-  
-  createStory(model: Story, isValid: boolean) { }
 
+    this.createStoryForm = new FormGroup({
+        storyId: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        number: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        storyName: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        Type: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        priority: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        label: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        resolution: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        description: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        attachment: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        userId: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        teamId: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+
+    });
+  }
 }
 
