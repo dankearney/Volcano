d0b1ec3967acb55c399303289298015b92fccdd8
Nicholas Teh <nicholasteh@dhcp-wifi-8021x-168-122-14-145.bu.edu>
2018-07-04 18:21:09 -0400

    Try to fix story creation

--- frontend/src/app/create-story/create-story.component.ts
+++ frontend/src/app/create-story/create-story.component.ts
@@ -25,18 +25,15 @@ export class CreateStoryComponent implements OnInit {
   ngOnInit() {
 
     this.createStoryForm = new FormGroup({
-        storyId: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         number: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         storyName: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
-        Type: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         priority: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
-        label: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        labels: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         resolution: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         description: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         attachment: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         userId: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         teamId: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
-
     });
   }
 
