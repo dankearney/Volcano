5d317e7f91685a343662b3b6dde7299dc2c6ce8c
qian <dingqiandd@gmail.com>
2018-07-04 21:19:35 -0400

    update create story in frontend

--- frontend/src/app/create-story/create-story.component.ts
+++ frontend/src/app/create-story/create-story.component.ts
@@ -25,29 +25,31 @@ export class CreateStoryComponent implements OnInit {
   ngOnInit() {
 
     this.createStoryForm = new FormGroup({
+        storyId: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        number: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         storyName: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        Type: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         priority: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
-        labels: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+        label: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         resolution: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         description: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         attachment: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         userId: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
         teamId: new FormControl('', [<any>Validators.required, <any>Validators.minLength(3)]),
+
     });
   }
 
-  createStory( model: Story, isValid: boolean) {
-    console.log("CODE RUNNING");
-    this.http.post("https://volcano-backend.herokuapp.com/story", model, Util.getReqConfig() ).subscribe(
-      data => {
-        Util.writeSuccess("Creation successful! Story Name, " + model.storyName);
-      },
-      err => {
-        Util.writeError("Story creation failed.");
-      }
-    );
-
+    createStory( model: Story, isValid: boolean) {
+      console.log("CODE RUNNING");
+      this.http.post("https://volcano-backend.herokuapp.com/stories", model, Util.getReqConfig() ).subscribe(
+        data => {
+          Util.writeSuccess("Creation successful! Story Name, " + model.storyName);
+        },
+        err => {
+          Util.writeError("Card creation failed.");
+        }
+      );
   }
-
 }
 
--- frontend/src/app/mock-story.ts
+++ frontend/src/app/mock-story.ts
@@ -3,15 +3,14 @@ import { Story } from './story';
 export const MOCK_STORY: Story =
 {
   storyId: 0,
+  number: 111,
   storyName: 'Create frontend',
-  cards: ['card name'],
+  Type: null,
   priority: 'high',
-  labels: 'mobile',
-  status: 'Not done',
+  label: 'mobile',
   resolution: '',
   description: 'First iteration of the frontend has the following requirements.\r\nBe responsive. Less than 200 ms lag.',
   attachment: '',
-  creator: 'Qian',
   userId: 1,
   teamId: 5,
 };
