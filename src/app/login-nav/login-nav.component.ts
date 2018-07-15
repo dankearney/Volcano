d5f2e591b069ee5aee096ff5ce0963d95bd8ad1b
dankearney <dank@LAPTOP-M7SECR63.localdomain>
2018-07-15 17:09:50 -0400

    set up team creation

--- src/app/login-nav/login-nav.component.ts
+++ src/app/login-nav/login-nav.component.ts
@@ -12,13 +12,16 @@ import { BrowserModule } from '@angular/platform-browser';
 export class LoginNavComponent implements OnInit {
 
   user: Object;
+  team: Object;
 
   constructor(public router: Router) { }
 
   refreshNavBar() {
   	this.user = Util.getLoggedInUser();
-    let team = Util.getCurrentTeam();
-    if (this.user != null && team == null && this.router.url != '/teams') {
+    this.team = Util.getCurrentTeam();
+    // If we're logged in, but there's no team, the user needs
+    // to pick one
+    if (this.user != null && this.team == null && this.router.url != '/teams') {
       this.router.navigate(['teams']);
     }
   }
