9c56bb0036e4fbd4ccbb8b6603763e8139defea8
Terence Blue <tblue@bu.edu>
2018-07-30 19:33:12 -0400

    More chat changes

--- frontend/src/app/chat-page/old-page/chat-page.component.spec.ts
+++ frontend/src/app/chat-page/old-page/chat-page.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChatPageComponent } from './chat-page.component';
 
+
 describe('ChatPageComponent', () => {
   let component: ChatPageComponent;
   let fixture: ComponentFixture<ChatPageComponent>;
