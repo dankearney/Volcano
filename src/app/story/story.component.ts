1c170f87d7472a435333f0bbe072e9c346497375
dankearney <danielpatrickkearney@gmail.com>
2018-07-30 21:20:40 -0400

    color code

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -10,10 +10,29 @@ import { Story } from '../story';
 export class StoryComponent implements OnInit {
 
   @Input() public story : Story;
+  ticketsInProgress = 0;
+  ticketsIncomplete = 0;
+  ticketsComplete = 0;
 
   constructor() { }
 
   ngOnInit() {
+  	console.log(this.story);
+  	if (this.story.cardsAttached != null) {
+  	  	for ( var i = 0; i<this.story.cardsAttached.length; i++ ) {
+  	  		var cardStatus = (this.story.cardsAttached[i])['resolution'];
+  	  		console.log(cardStatus);
+  	  		if (new String(cardStatus).valueOf() == new String('Incomplete').valueOf()) {
+  	  			this.ticketsIncomplete += 1;
+  	  		}
+  	  		if (new String(cardStatus).valueOf() == new String('In Progress').valueOf()) {
+  	  			this.ticketsInProgress += 1;
+  	  		}
+  	  		if (new String(cardStatus).valueOf() == new String('Complete').valueOf()) {
+  	  			this.ticketsComplete += 1;
+  	  		}
+  	  	}
+  	}
   }
 
 }
